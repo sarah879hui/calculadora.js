823ecfe9a5a24d05f336fd077d97bda981a09871
sarah <ss8150348@gmail.com>
2026-09-04 16:09:07 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,19 @@
+let n1 = Number(prompt("Digite um número:"))
+let n2 = Number (prompt("Digite outro número:"))
+let soma = n1 + n2
+console.log(soma)
+
+let n1 = Number(prompt("Digite um número:"))
+let n2 = Number (prompt("Digite outro número:"))
+let soma = n3 - n4
+console.log(subtração)
+
+let n1 = Number(prompt("Digite um número:"))
+let n2 = Number (prompt("Digite outro número:"))
+let soma = n5 * n6
+console.log(multiplicação)
+
+let n1 = Number(prompt("Digite um número:"))
+let n2 = Number (prompt("Digite outro número:"))
+let soma = n7/n8
+console.log(divisão)
